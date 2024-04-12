0a6ff765616670d34295e3722e778ad0be3d1e84
Cyntiche Nguemete <cyntichenguemete@gmail.com>
2024-04-12 15:36:20 +0100

    ci(app): #4  install and configure flowbite for responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 
 import { colors } from "./public/assets/Colors.js";
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       colors: {
@@ -10,5 +12,7 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 };
