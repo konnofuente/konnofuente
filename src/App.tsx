840d21e836b1c00881b7903b8f2a7374096d1da6
Cyntiche Nguemete <cyntichenguemete@gmail.com>
2024-04-12 09:59:36 +0100

    fix(app): #4 change directory of header components

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { IntlProvider } from 'react-intl';
 import enMessages from './lang/enMessages';
 import frMessages from './lang/frMessages';
-import Header from './components/Header';
+import Header from './components/Header/Header';
 
 function App() {
   const [activeLanguage, setActiveLanguage] = useState("fr");
