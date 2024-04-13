85720ffeed529858e5e618a564a8111215b9001e
Cyntiche Nguemete <cyntichenguemete@gmail.com>
2024-04-13 22:36:27 +0100

    feat(app): #6 setup title and subtitle

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
-import { IntlProvider } from 'react-intl';
-import enMessages from './lang/enMessages';
-import frMessages from './lang/frMessages';
-import Header from './components/Header/Header';
+import React, { useState } from "react";
+import { IntlProvider } from "react-intl";
+import enMessages from "./lang/enMessages";
+import frMessages from "./lang/frMessages";
+import Header from "./components/Header/Header";
+import TitleAndSubtitle from "./components/TitleAndSubtitle/TitleAndSubtitle";
 
 function App() {
   const [activeLanguage, setActiveLanguage] = useState("fr");
@@ -12,10 +13,18 @@ function App() {
   return (
     <IntlProvider locale={activeLanguage} messages={messages}>
       <div>
-        <Header/>
-      <button onClick={() => setActiveLanguage(prev => prev === "fr" ? "en" : "fr")}>
-        {`Change language: ${activeLanguage}`}
-      </button>
+        <Header />
+        <TitleAndSubtitle
+          title={"ABOUT US"}
+          subtitle={"Get to know more about me and my values"}
+        />
+        <button
+          onClick={() =>
+            setActiveLanguage((prev) => (prev === "fr" ? "en" : "fr"))
+          }
+        >
+          {`Change language: ${activeLanguage}`}
+        </button>
       </div>
     </IntlProvider>
   );
