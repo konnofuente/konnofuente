68d3365c5762e71add1c800a9ba4c3ccb4be3fae
Cyntiche Nguemete <cyntichenguemete@gmail.com>
2024-04-13 22:36:38 +0100

    feat(app): #4 header desktop done

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -18,23 +18,24 @@ export default function Header() {
 
   return (
     <div className="header-section">
-      <div className="bg-primaryNormal flex py-2.5 px-32 justify-between items-center">
-          <div>
-            <img src={konnoLogo} alt="Logo" className="h-114 mr-115" />
+      <header>
+        <nav className=" bg-primaryNormal flex  py-2.5  px-32  justify-between items-center sm:py-2.5 ">
+          <div className="w-[114px] md:w-[114px]">
+            <img src={konnoLogo} alt="Logo" />
           </div>
-          <div>
-            <ul className="flex py-20px items-center gap-24">
-            {navItems.map((navItem, index) => (
-              <li  key={index}>
-                <span className="text-white hover:text-gray-300">
-                {navItem.item}
-                </span>
-              </li>
+          <div className="md:static md:w-auto md:h-auto md:items-center md:py-0">
+            <ul className="flex py-20px items-center gap-24 md:flex-row md:items-center md:gap-[2vw]">
+              {navItems.map((navItem, index) => (
+                <li key={index} className="">
+                  <span className="text-white font-raleway text-base font-normal leading-normal hover:text-gray-300">
+                    {navItem.item}
+                  </span>
+                </li>
               ))}
-              
             </ul>
           </div>
-      </div>
+        </nav>
+      </header>
       <span>{formatMessage({ id: "bonjour" })}</span>
     </div>
   );
