f2ef6cd33e26b64b5e94c907b47966906531cc9c
Nguemete Cyntiche <cyntichenguemete@gmail.com>
2024-04-15 17:03:02 +0100

    feat(app): #8 set button without icon

--- src/components/base/buttons/ButtonWithoutIcon.tsx
+++ src/components/base/buttons/ButtonWithoutIcon.tsx
@@ -1,18 +1,16 @@
 import React from "react";
-import iconRight from "../../../assets/icons/icons_right-solid.png";
 
 interface ButtonWithIconProps {
   textButton: string;
 }
-const Button: React.FC<ButtonWithIconProps> = ({ textButton }) => {
+const ButtonWithoutIcon: React.FC<ButtonWithIconProps> = ({ textButton }) => {
   return (
     <div className="flex justify-center items-center w-[220px] h-[64px] gap-[10px] rounded-lg bg-primaryNormal">
       <p className="text-WhiteNormal text-[20px] text-center font-Raleway font-bold text-whiteNormal">
         CONTACT ME
       </p>
-      <img src={iconRight} alt="Icon right"></img>
     </div>
   );
 };
 
-export default Button;
+export default ButtonWithoutIcon;
