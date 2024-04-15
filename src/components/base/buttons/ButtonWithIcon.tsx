03d98ecd5f7817ee7b3092bcd58a94efeae6c8da
Nguemete Cyntiche <cyntichenguemete@gmail.com>
2024-04-15 19:31:00 +0100

    refactor(app): #9 refractor button

--- src/components/base/buttons/ButtonWithIcon.tsx
+++ src/components/base/buttons/ButtonWithIcon.tsx
@@ -8,7 +8,7 @@ const ButtonWithIcon: React.FC<ButtonWithIconProps> = ({ textButton }) => {
   return (
     <div className="flex justify-center items-center w-[220px] h-[64px] gap-[10px] sm:rounded-lg bg-primaryNormal">
       <p className="text-WhiteNormal text-[20px] text-center font-Raleway font-bold text-whiteNormal">
-        CONTACT ME
+        {textButton}
       </p>
       <img src={iconRight} alt="Icon right"></img>
     </div>
