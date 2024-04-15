b42252dae72539f7b698315d916149c31142bc2b
Nguemete Cyntiche <cyntichenguemete@gmail.com>
2024-04-15 19:33:13 +0100

    fix(app): # fix small bug

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -19,12 +19,12 @@ export default function Header() {
   return (
     <div className="header-section">
       <header>
-        <nav className=" bg-primaryNormal flex  py-2.5  px-32  justify-between items-center sm:py-2.5 ">
-          <div className="w-[114px] md:w-[114px]">
+        <nav className=" bg-primaryNormal flex  py-2.5  px-32  justify-between items-center ">
+          <div>
             <img src={konnoLogo} alt="Logo" />
           </div>
-          <div className="md:static md:w-auto md:h-auto md:items-center md:py-0">
-            <ul className="flex py-20px items-center gap-24 md:flex-row md:items-center md:gap-[2vw]">
+          <div>
+            <ul className=" hidden md:flex  py-20px items-center gap-24 ">
               {navItems.map((navItem, index) => (
                 <li key={index} className="">
                   <span className="text-white font-raleway text-base font-normal leading-normal hover:text-gray-300">
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,18 +1,20 @@
-import { useState } from "react";
-import TitleAndSubtitle from "../components/base/TitleAndSubtitle/TitleAndSubtitle";
-import enMessages from "../lang/enMessages";
-import frMessages from "../lang/frMessages";
-import konnoImg from "../assets/images/konnoImg.jpg";
-import ButtonWithIcon from "../components/base/buttons/ButtonWithIcon";
-import ButtonWithoutIcon from "../components/base/buttons/ButtonWithoutIcon";
+// import { useState } from "react";
+// import TitleAndSubtitle from "../components/base/TitleAndSubtitle/TitleAndSubtitle";
+// import enMessages from "../lang/enMessages";
+// import frMessages from "../lang/frMessages";
+// import konnoImg from "../assets/images/konnoImg.jpg";
+// import ButtonWithIcon from "../components/base/buttons/ButtonWithIcon";
+// import ButtonWithoutIcon from "../components/base/buttons/ButtonWithoutIcon";
+import Hero from "../features/hero/hero";
 
 function LandingPage() {
-  const [activeLanguage, setActiveLanguage] = useState("fr");
-  const messages = activeLanguage === "en" ? enMessages : frMessages;
+  // const [activeLanguage, setActiveLanguage] = useState("fr");
+  // const messages = activeLanguage === "en" ? enMessages : frMessages;
 
   return (
     <div>
-      <TitleAndSubtitle
+      <Hero />
+      {/* <TitleAndSubtitle
         title={"ABOUT US"}
         subtitle={"Get to know more about me and my values"}
       />
@@ -29,7 +31,7 @@ function LandingPage() {
           className="w-340 h-340 rounded-full border-solid border-[102px] border-primaryNormal"
         />
         <ButtonWithIcon textButton="CONTACT ME"/>
-        <ButtonWithoutIcon textButton="CONTACT ME"/>
+        <ButtonWithoutIcon textButton="CONTACT ME"/> */}
     </div>
   );
 }
