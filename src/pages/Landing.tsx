804ab0d3ef26526e2d1809e7230ac5807801bc28
Nguemete Cyntiche <cyntichenguemete@gmail.com>
2024-04-15 17:16:09 +0100

    test(app): #8 test display of button

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -3,6 +3,8 @@ import TitleAndSubtitle from "../components/base/TitleAndSubtitle/TitleAndSubtit
 import enMessages from "../lang/enMessages";
 import frMessages from "../lang/frMessages";
 import konnoImg from "../assets/images/konnoImg.jpg";
+import ButtonWithIcon from "../components/base/buttons/ButtonWithIcon";
+import ButtonWithoutIcon from "../components/base/buttons/ButtonWithoutIcon";
 
 function LandingPage() {
   const [activeLanguage, setActiveLanguage] = useState("fr");
@@ -26,6 +28,8 @@ function LandingPage() {
           alt="Hero image"
           className="w-340 h-340 rounded-full border-solid border-[102px] border-primaryNormal"
         />
+        <ButtonWithIcon textButton="CONTACT ME"/>
+        <ButtonWithoutIcon textButton="CONTACT ME"/>
     </div>
   );
 }
