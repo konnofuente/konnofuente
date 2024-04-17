ed7f324c8f6f6eb779118b4c59a5b0c5e61fe4a1
Nguemete Cyntiche <cyntichenguemete@gmail.com>
2024-04-17 10:05:20 +0100

    perf(app): #11 Add Translation to about me section

--- src/features/aboutMe/AboutMe.tsx
+++ src/features/aboutMe/AboutMe.tsx
@@ -1,8 +1,6 @@
-import React from "react";
+import { useTranslation } from "react-i18next";
 import aboutMeImg from "../../assets/images/aboutMeImg.png";
 import TitleAndSubtitle from "../../components/base/TitleAndSubtitle/TitleAndSubtitle";
-import { useTranslation } from "react-i18next";
-import SwitchLangBtn from "../../components/base/buttons/SwitchLangBtn";
 
 const AboutMe = () => {
   const [t, i18n] = useTranslation("message");
@@ -10,28 +8,16 @@ const AboutMe = () => {
   return (
     <div className=" flex flex-col items-center bg-whiteNormal px-24 py-32 gap-y-16">
       <TitleAndSubtitle
-        title={"ABOUT ME"}
-        subtitle={"Get to know more about me and my values"}
+        title={t("aboutMe.aboutMeTitle")}
+        subtitle={t("aboutMe.aboutMeSubtitle")}
       />
       <div className="flex justify-between items-center w-full">
         <div className="flex flex-col  items-start ">
           <h1 className="font-Raleway text-blackNormal text-xl font-extrabold uppercase">
-            {t("about.salut")}
-          </h1>
-          <SwitchLangBtn />
-          <h1 className="font-Raleway text-blackNormal text-xl font-extrabold uppercase">
-            Meet the Mind Behind Fuente !
+            {t("aboutMe.meetTheMind")}
           </h1>
           <p className="font-Raleway text-whiteDarker text-lg font-bold leading-[30px] w-3/4">
-            I'm Konno Meli Arnaud,widely recognized as Fuente, a seasoned
-            frontend developer from Cameroon. Proficient in Figma, Adobe
-            Products, and Affinity Design, I blend technical skills with design
-            acumen to create captivating user interfaces. Along with strong
-            leadership and professional ethics, I prioritize time management and
-            deadline adherence in all my projects. This commitment ensures not
-            only quality but also reliability. If you value punctuality and
-            dedication, we'll work exceptionally well together. Connect with me
-            on LinkedIn.
+            {t("aboutMe.description")}
           </p>
         </div>
         <img src={aboutMeImg} alt="About Me" />
