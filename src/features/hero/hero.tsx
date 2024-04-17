d60f2b936eaf24997a586ff0e4f2ef69ae0e4f5e
Nguemete Cyntiche <cyntichenguemete@gmail.com>
2024-04-17 10:34:41 +0100

    perf(app): #6 add traduction to hero feature

--- src/features/hero/hero.tsx
+++ src/features/hero/hero.tsx
@@ -1,24 +1,23 @@
 import konnoImg from "../../assets/images/konnoImg.jpg";
 import ButtonWithIcon from "../../components/base/buttons/ButtonWithIcon";
 import iconRight from "../../assets/icons/icons_right-solid.png";
-
+import { useTranslation } from "react-i18next";
 
 const Hero = () => {
+  const [t] = useTranslation("message");
   return (
     <div className=" flex flex-wrap items-center justify-between bg-secondaryNormal px-24 py-32">
       <div className="flex flex-col items-start gap-5">
         <div className="inline-flex flex-col items-start">
           <h1 className="font-Raleway text-primaryNormal text-5xl font-extrabold leading-[70px] uppercase ">
-            Frontend dev.
+            {t("hero.post")}
           </h1>
           <h1 className="font-Raleway text-whiteNormal text-5xl font-extrabold leading-[70px] uppercase ">
-            ui/ux skill.
+            {t("hero.skill")}
           </h1>
         </div>
         <p className="font-Raleway text-whiteNormal text-lg font-bold leading-[30px] w-96">
-          I specialize in crafting scalable frontend solutions with top-notch
-          user experiences. My focus on clean code and client feedback ensures
-          quality in every project.
+          {t("hero.description")}
         </p>
         <div className=" flex flex-wrap justify-between gap-5 items-start">
           <ButtonWithIcon textButton="CONTACT ME" iconRight={iconRight} />
