0040c43e7b5ea7543ac8e6e1d2eac1b03bcc1a1c
konnofuente <konomelifuente@gmail.com>
2024-04-19 14:53:47 +0100

    refactor(app): #11 updated about-us responsiveness

--- src/features/aboutMe/AboutMe.tsx
+++ src/features/aboutMe/AboutMe.tsx
@@ -6,21 +6,24 @@ const AboutMe = () => {
   const [t, i18n] = useTranslation("message");
 
   return (
-    <div className=" flex flex-col sm:justify-between items-center bg-whiteNormal md:px-24 md:py-32 py-16 px-2 gap-y-16 w-full">
+    <div className=" flex flex-col  sm:justify-between items-center bg-whiteNormal md:px-10 lg:px-32 md:py-16 py-16 px-2 gap-10 w-full">
       <TitleAndSubtitle
         title={t("aboutMe.aboutMeTitle")}
         subtitle={t("aboutMe.aboutMeSubtitle")}
       />
-      <div className="flex justify-between items-center w-full ">
-        <div className="flex flex-col  items-start ">
-          <h1 className="font-Raleway text-blackNormal text-xl font-extrabold uppercase">
+      <div className="grid justify-between items-center w-full gap-10 lg:flex lg:flex-row lg:items-center">
+        <div className="flex flex-col  items-start gap-5 md:gap-10 ">
+          <h1 className="font-Raleway text-blackNormal text-xl lg:text-3xl font-extrabold uppercase">
             {t("aboutMe.meetTheMind")}
           </h1>
-          <p className="font-Raleway sm:text-left text-center text-whiteDarker text-lg font-bold leading-[30px] w-3/4">
+          <p className="font-Raleway  text-left text-whiteDarker lg:text-xl lg:w-[500px] text-xs md:text-lg font-semibold leading-6 ">
             {t("aboutMe.description")}
           </p>
         </div>
+        <div className="flex items-center justify-center">
+
         <img src={aboutMeImg} alt="About Me" />
+        </div>
       </div>
     </div>
   );
