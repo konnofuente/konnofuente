5f24b93e1d458ed250764c80ba392a864f790195
konnofuente <konomelifuente@gmail.com>
2024-07-16 11:57:35 +0100

    refactor(app): #22 data and type in useAboutFeature hook

--- src/features/about_me/components/AboutPage.tsx
+++ src/features/about_me/components/AboutPage.tsx
@@ -4,64 +4,20 @@ import ImageSlider from "./ImageSlider";
 import Title from "../../../shared/components/Title";
 import { TitleSize } from "../../../shared/utils/constants/enum/TitleSize";
 import ImageTextSection from "../../../shared/components/section/ImageTextSection";
-import assets from "../../../assets/assets";
-
-type AboutSection = {
-  title: string;
-  description: string;
-  image: string;
-  imageLeft: boolean;
-};
-
-const dailySections: AboutSection[] = [
-  {
-    title: "Day Scheduling",
-    description:
-      "I start my day at 3 AM by scheduling and planning. I check my emails, ensure all tasks are updated in Google Calendar, and organize my day and team activities using Trello. I set up my workspace with GitHub, VS Code, and Figma. With a coffee by my side, I’m ready to start the day.",
-    image: assets.Images.scheduleChart,
-    imageLeft: false,
-  },
-  {
-    title: "Coding",
-    description:
-      "Following my Trello plan, I open VS Code and dive into coding with my Rize app open for productivity. I work on web applications using ReactJS or VueTS, or I’m immersed in a mobile application project, which I particularly enjoy with Flutter and React Native.",
-    image: assets.Images.myStackPc,
-    imageLeft: true,
-  },
-  {
-    title: "Seeking Knowledge",
-    description:
-      "After several hours of work, I take a break to seek new knowledge. I open LinkedIn to share tips or engage with courses and books on software architecture, clean coding, or other topics to enhance my skills. Alternatively, I relax with YouTube videos on sports or other interesting topics.",
-    image: assets.Images.seekingKnowledge,
-    imageLeft: false,
-  }
-];
-
-const finalizeSections: AboutSection[] = [
-  {
-    title: "Passion for Entrepreneurship",
-    description:
-      "I have a deep interest in the entrepreneurial lifestyle. I am passionate about creating and growing innovative solutions. I enjoy brainstorming new ideas, working on business strategies, and networking with other entrepreneurs. This entrepreneurial spirit drives me to continuously improve and stay ahead in the tech industry.",
-    image: assets.Images.entrepreneurPassion,
-    imageLeft: false,
-  },
-  {
-    title: "Weekends and Leisure",
-    description:
-      "On weekends, I love to travel and explore new places. My most fancy hobby is baking and cooking, and I enjoy making vlogs about it to share my culinary adventures. I spend quality time with friends, whether it's visiting new spots, watching sports matches together, or just hanging out. These activities help me relax and recharge for the upcoming week.",
-    image: assets.Images.weekendLeisure,
-    imageLeft: false,
-  }
-];
+import { useAboutFeature } from "../hooks/useAboutFeature";
 
 function AboutPage() {
+  const { finalizeSections, dailySections } = useAboutFeature();
+
   return (
     <div className="gradient-background min-h-screen overflow-hidden">
       <Hero />
       <ImageSlider />
       <div className="flex justify-center p-4">
-
-      <div className="text-sky-950 text-[35px] sm:text-[45px] lg:text-[70px] text-center font-bold font-['Helvetica']"> Let Me Take You Through My Day</div>
+        <div className="text-sky-950 text-[35px] sm:text-[45px] lg:text-[70px] text-center font-bold font-['Helvetica']">
+          {" "}
+          Let Me Take You Through My Day
+        </div>
       </div>
 
       {dailySections.map((section, index) => (
